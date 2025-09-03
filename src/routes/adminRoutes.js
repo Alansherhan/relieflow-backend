56a5273b3738adac5ba4d9cfa2d353d108836b11
Alansherhan <alansherhan10@gmail.com>
2025-09-03 13:46:25 +0530

    Modified

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,15 +1,10 @@
 import { group } from "../utils/routerUtils.js";
 
 import { adminSignUp } from "../controllers/adminUserController.js";
-import { getAllCalamityTypes, addCalamity, deleteCalamityType } from "../controllers/calamityTypeController.js";
+import { getAllCalamityTypes, addCalamity } from "../controllers/calamityTypeController.js";
 import { getAllAidRequests, addAidRequest, getAidRequest, deleteAidRequest } from "../controllers/aidRequestController.js";
-<<<<<<< HEAD
-import { assignTask, getAllTasks } from "../controllers/taskController.js";
-import { addCenter, deleteReliefCenter, getAllReliefCenters, getReliefCenter } from "../controllers/reliefCenterController.js";
-=======
 import { assignTask, deletTask, getAllTasks } from "../controllers/taskController.js";
 import { addCenter, deleteReliefCenter, getAllReliefCenters } from "../controllers/reliefCenterController.js";
->>>>>>> e0510bf42addcc2efaba62d3e0fc6bd723dc17d2
 
 
 export function adminRoutes(router) {
@@ -19,7 +14,6 @@ export function adminRoutes(router) {
         (calamityRouter) => {
             calamityRouter.post("/add", addCalamity);
             calamityRouter.get('/', getAllCalamityTypes);
-            calamityRouter.delete('/delete/:id',deleteCalamityType);
         },
         router,
     );
@@ -46,7 +40,6 @@ export function adminRoutes(router) {
         '/center',
         (centerRouter)=>{
             centerRouter.post("/add",addCenter)
-            centerRouter.get("/:id",getReliefCenter)
             centerRouter.get("/",getAllReliefCenters)
             centerRouter.delete("/delete/:id",deleteReliefCenter)
         },
