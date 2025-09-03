dde363c27cab2b54c52830a36b429640e9677861
Alansherhan <alansherhan10@gmail.com>
2025-09-03 13:09:41 +0530

    modified

--- src/routes/publicUser.routes.js
+++ src/routes/publicUser.routes.js
@@ -1,7 +1,7 @@
 import { login, signUp } from "../controllers/userProfileController.js"
 import { addDonationRequest, deleteDonationRequest, getAllDonationRequests } from "../controllers/donationRequestController.js"
 import { addDonation, getAllDonations } from "../controllers/donationController.js"
-import {  protect } from "../middleWare/authMiddleware.js"
+// import {  protect } from "../middleWare/authMiddleware.js"
 import { group } from "../utils/routerUtils.js"
 
 export function publicUserRoutes(router) {
