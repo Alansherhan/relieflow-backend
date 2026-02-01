489964c32ded32b29c0e2d2c7ef956ff970e22a4
Alansherhan <alansherhan10@gmail.com>
2026-02-01 18:18:07 +0530

    feat: extend session cookie expiration to 30 days and add proof image for uploads

--- server.js
+++ server.js
@@ -152,7 +152,7 @@ const sessionMiddleware = session({
   cookie: {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    maxAge: 1000 * 60 * 60 * 24,
+    maxAge: 1000 * 60 * 60 * 24 * 30                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       ,
   },
   name: 'adminjs-session',
 });
